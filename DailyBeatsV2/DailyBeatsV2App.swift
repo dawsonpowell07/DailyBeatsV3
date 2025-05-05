2635be0fd48e90cfa70543fcf82004860e7e39b5
Dawson <dawsonpowell07@gmail.com>
2025-05-05 15:09:12 -0400

    Initial Commit

---
+++ DailyBeatsV2/DailyBeatsV2App.swift
@@ -0,0 +1,17 @@
+//
+//  DailyBeatsV2App.swift
+//  DailyBeatsV2
+//
+//  Created by Dawson Powell on 5/5/25.
+//
+
+import SwiftUI
+
+@main
+struct DailyBeatsV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DailyBeatsV2Tests/DailyBeatsV2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  DailyBeatsV2Tests.swift
+//  DailyBeatsV2Tests
+//
+//  Created by Dawson Powell on 5/5/25.
+//
+
+import Testing
+@testable import DailyBeatsV2
+
+struct DailyBeatsV2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
